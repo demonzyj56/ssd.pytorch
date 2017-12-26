1aac87498d73f72b33fdcb111a42dae1716a0643
demonzyj56 <demonzyj56@gmail.com>
2017-12-26 19:28:01 +0800

    Reasonable setting to parse from parser

--- train_vid.py
+++ train_vid.py
@@ -19,13 +19,13 @@ def str2bool(v):
     return v.lower() in ("yes", "true", "t", "1")
 
 parser = argparse.ArgumentParser(description='Single Shot MultiBox Detector Training')
-parser.add_argument('--size', default=300, type=int, help='use 300 or 512 as base size')
-parser.add_argument('--version', default='v2', help='conv11_2(v2) or pool6(v1) as last layer')
+parser.add_argument('--size', default=512, type=int, help='use 300 or 512 as base size')
+parser.add_argument('--version', default='v2_512', help='conv11_2(v2) or pool6(v1) as last layer')
 parser.add_argument('--basenet', default='vgg16_reducedfc.pth', help='pretrained base model')
 parser.add_argument('--jaccard_threshold', default=0.5, type=float, help='Min Jaccard index for matching')
 parser.add_argument('--batch_size', default=16, type=int, help='Batch size for training')
 parser.add_argument('--resume', default=None, type=str, help='Resume from checkpoint')
-parser.add_argument('--num_workers', default=2, type=int, help='Number of workers used in dataloading')
+parser.add_argument('--num_workers', default=4, type=int, help='Number of workers used in dataloading')
 parser.add_argument('--iterations', default=120000, type=int, help='Number of training iterations')
 parser.add_argument('--start_iter', default=0, type=int, help='Begin counting iterations starting from this value (should be used with resume)')
 parser.add_argument('--cuda', default=True, type=str2bool, help='Use cuda to train model')
@@ -51,7 +51,7 @@ cfg = v2_512 if args.version == 'v2_512' else v2
 if not os.path.exists(args.save_folder):
     os.mkdir(args.save_folder)
 
-train_sets = ['DET_train_30classes', 'VID_train_15frames']
+train_sets = ['DET_train_30classes','VID_train_15frames']
 
 ssd_dim = args.size
 means = (104, 117, 123)  # only support voc now
@@ -59,11 +59,11 @@ num_classes = len(VID_CLASSES) + 1
 batch_size = args.batch_size
 accum_batch_size = 32
 iter_size = accum_batch_size / batch_size
-max_iter = 100
+max_iter = args.iterations
 weight_decay = 0.0005
 stepvalues = (80000, 100000, 120000)
-gamma = 0.1
-momentum = 0.9
+gamma = args.gamma
+momentum = args.momentum
 
 if args.visdom:
     import visdom
@@ -75,6 +75,9 @@ net = ssd_net
 if args.cuda:
     #  net = torch.nn.DataParallel(ssd_net)
     cudnn.benchmark = True
+else:
+    print('not using cuda now!')
+
 
 if args.resume:
     print('Resuming training, loading {}...'.format(args.resume))
@@ -221,7 +224,7 @@ def train():
             torch.save(ssd_net.state_dict(), 'weights/ssd{}_vid_{}.pth'.format(
                 args.size, iteration
             ))
-    torch.save(ssd_net.state_dict(), args.save_folder + '' + args.version + '.pth')
+    torch.save(ssd_net.state_dict(), args.save_folder + 'vid_ssd_' + args.version + '.pth')
 
 
 def adjust_learning_rate(optimizer, gamma, step):
