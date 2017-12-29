aa23602375de9c4d4e721e19d6fc0a9d4bda5832
demonzyj56 <demonzyj56@gmail.com>
2017-12-29 10:12:42 +0800

    Add training script for accumulating gradients for multiple steps.

---
+++ train_vid_video_accum.py
@@ -0,0 +1,270 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+import torch.nn.init as init
+import argparse
+from torch.autograd import Variable
+import torch.utils.data as data
+from data import v2_512, v2, detection_collate, VOCroot
+from data import VIDDetection, VIDroot, VID_CLASSES, VIDVideoDetection
+from utils.augmentations_video import SSDAugmentationVideo
+from layers.modules import MultiBoxLoss
+from ssd import build_ssd
+from ssd_video import build_ssd_video
+import numpy as np
+import time
+
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1")
+
+parser = argparse.ArgumentParser(description='Single Shot MultiBox Detector Training')
+parser.add_argument('--size', default=512, type=int, help='use 300 or 512 as base size')
+parser.add_argument('--version', default='v2_512', help='conv11_2(v2) or pool6(v1) as last layer')
+parser.add_argument('--basenet', default='vgg16_reducedfc.pth', help='pretrained base model')
+parser.add_argument('--jaccard_threshold', default=0.5, type=float, help='Min Jaccard index for matching')
+parser.add_argument('--batch_size', default=16, type=int, help='Batch size for training')
+parser.add_argument('--iter_size', default=1, type=int, help='Iter size to accumulate gradient values')
+parser.add_argument('--resume', default=None, type=str, help='Resume from checkpoint')
+parser.add_argument('--num_workers', default=4, type=int, help='Number of workers used in dataloading')
+parser.add_argument('--iterations', default=120000, type=int, help='Number of training iterations')
+parser.add_argument('--start_iter', default=0, type=int, help='Begin counting iterations starting from this value (should be used with resume)')
+parser.add_argument('--cuda', default=True, type=str2bool, help='Use cuda to train model')
+parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float, help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+parser.add_argument('--weight_decay', default=5e-4, type=float, help='Weight decay for SGD')
+parser.add_argument('--gamma', default=0.1, type=float, help='Gamma update for SGD')
+parser.add_argument('--log_iters', default=True, type=bool, help='Print the loss at each iteration')
+parser.add_argument('--visdom', default=False, type=str2bool, help='Use visdom to for loss visualization')
+parser.add_argument('--send_images_to_visdom', type=str2bool, default=False, help='Sample a random image from each 10th batch, send it to visdom after augmentations step')
+parser.add_argument('--save_folder', default='weights/', help='Location to save checkpoint models')
+parser.add_argument('--voc_root', default=VOCroot, help='Location of VOC root directory')
+parser.add_argument('--seed', default=-1, type=int, help='Random seed to set, negative values means not setting')
+parser.add_argument('--K', default=0, help='The size of video context')
+args = parser.parse_args()
+
+# Ensure determinism
+if args.seed >= 0:
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if args.cuda:
+        torch.cuda.manual_seed(args.seed)
+
+if args.cuda and torch.cuda.is_available():
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+else:
+    torch.set_default_tensor_type('torch.FloatTensor')
+
+#  cfg = (v1, v2)[args.version == 'v2']
+cfg = v2_512 if args.version == 'v2_512' else v2
+
+if not os.path.exists(args.save_folder):
+    os.mkdir(args.save_folder)
+
+train_sets = ['DET_train_30classes']  #,'VID_train_K_1']
+
+ssd_dim = args.size
+means = (104, 117, 123)  # only support voc now
+num_classes = len(VID_CLASSES) + 1
+batch_size = args.batch_size
+# accum_batch_size = 32
+# iter_size = accum_batch_size / batch_size
+iter_size = args.iter_size
+max_iter = args.iterations
+weight_decay = 0.0005
+stepvalues = (160000, 200000, 240000)
+gamma = args.gamma
+momentum = args.momentum
+
+if args.visdom:
+    import visdom
+    viz = visdom.Visdom()
+
+ssd_net = build_ssd_video('train', ssd_dim, num_classes, args.K)
+net = ssd_net
+
+if args.cuda:
+    #  net = torch.nn.DataParallel(ssd_net)
+    cudnn.benchmark = True
+else:
+    print('not using cuda now!')
+
+
+if args.resume:
+    print('Resuming training, loading {}...'.format(args.resume))
+    ssd_net.load_weights(args.resume)
+else:
+    vgg_weights = torch.load(args.save_folder + args.basenet)
+    print('Loading base network...')
+    ssd_net.vgg.load_state_dict(vgg_weights)
+
+if args.cuda:
+    net = net.cuda()
+
+
+def xavier(param):
+    init.xavier_uniform(param)
+
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d):
+        xavier(m.weight.data)
+        m.bias.data.zero_()
+
+
+if not args.resume:
+    print('Initializing weights...')
+    # initialize newly added layers' weights with xavier method
+    ssd_net.extras.apply(weights_init)
+    ssd_net.loc.apply(weights_init)
+    ssd_net.conf.apply(weights_init)
+
+optimizer = optim.SGD(net.parameters(), lr=args.lr,
+                      momentum=args.momentum, weight_decay=args.weight_decay)
+criterion = MultiBoxLoss(num_classes, 0.5, True, 0, True, 3, 0.5, False, args.cuda,
+                         size_average=False)
+
+
+def train():
+    net.train()
+    # loss counters
+    loc_loss = 0  # epoch
+    conf_loss = 0
+    epoch = 0
+    print('Loading Dataset...')
+
+    # parameters for accumulating losses for minibatches
+    iter_count = 0
+    pos_count = 0
+    losses = 0
+
+    dataset = VIDVideoDetection(train_sets, 'data/', VIDroot, transform=SSDAugmentationVideo(
+        ssd_dim, means
+    ), dataset_name='ImageNetVID', is_test=False, k=args.K)
+
+    epoch_size = len(dataset) // args.batch_size
+    print('Training SSD on', dataset.name)
+    step_index = 0
+    if args.visdom:
+        # initialize visdom loss plot
+        lot = viz.line(
+            X=torch.zeros((1,)).cpu(),
+            Y=torch.zeros((1, 3)).cpu(),
+            opts=dict(
+                xlabel='Iteration',
+                ylabel='Loss',
+                title='Current SSD Training Loss',
+                legend=['Loc Loss', 'Conf Loss', 'Loss']
+            )
+        )
+        epoch_lot = viz.line(
+            X=torch.zeros((1,)).cpu(),
+            Y=torch.zeros((1, 3)).cpu(),
+            opts=dict(
+                xlabel='Epoch',
+                ylabel='Loss',
+                title='Epoch SSD Training Loss',
+                legend=['Loc Loss', 'Conf Loss', 'Loss']
+            )
+        )
+    batch_iterator = None
+    data_loader = data.DataLoader(dataset, batch_size, num_workers=args.num_workers,
+                                  shuffle=True, collate_fn=detection_collate, pin_memory=True)
+    for iteration in range(args.start_iter, max_iter):
+        if (not batch_iterator) or (iteration % epoch_size == 0):
+            # create batch iterator
+            batch_iterator = iter(data_loader)
+        if iteration in stepvalues:
+            step_index += 1
+            adjust_learning_rate(optimizer, args.gamma, step_index)
+            if args.visdom:
+                viz.line(
+                    X=torch.ones((1, 3)).cpu() * epoch,
+                    Y=torch.Tensor([loc_loss, conf_loss,
+                        loc_loss + conf_loss]).unsqueeze(0).cpu() / epoch_size,
+                    win=epoch_lot,
+                    update='append'
+                )
+            # reset epoch loss counters
+            loc_loss = 0
+            conf_loss = 0
+            epoch += 1
+
+        # load train data
+        images, targets = next(batch_iterator)
+        images = images.view(-1, 3, images.size(-2), images.size(-1))
+
+        if args.cuda:
+            images = Variable(images.cuda())
+            targets = [Variable(anno.cuda(), volatile=True) for anno in targets]
+        else:
+            images = Variable(images)
+            targets = [Variable(anno, volatile=True) for anno in targets]
+        # forward
+        t0 = time.time()
+        out = net(images)
+        # backprop
+        # loss_l, loss_c = criterion(out, targets)
+        # loss = loss_l + loss_c
+        # loss.backward()
+        loss_l, loss_c, N = criterion(out, targets)
+        print('loss_l: {:3f}, loss_c: {:3f}'.format(loss_l, loss_c))
+        loss = (loss_l + loss_c) / N
+        losses += loss_l + loss_c
+        iter_count += 1
+        pos_count += N
+        if iter_count == args.iter_size or iteration == max_iter - 1:
+            optimizer.zero_grad()
+            losses /= pos_count
+            losses.backward()
+            optimizer.step()
+            iter_count = 0
+            pos_count = 0
+            losses = 0
+        t1 = time.time()
+        loc_loss += loss_l.data[0] / N
+        conf_loss += loss_c.data[0] / N
+        if iteration % 10 == 0:
+            print('Timer: %.4f sec.' % (t1 - t0))
+            print('iter ' + repr(iteration) + ' || Loss: %.4f ||' % (loss.data[0]), end=' ')
+            if args.visdom and args.send_images_to_visdom:
+                random_batch_index = np.random.randint(images.size(0))
+                viz.image(images.data[random_batch_index].cpu().numpy())
+        if args.visdom:
+            viz.line(
+                X=torch.ones((1, 3)).cpu() * iteration,
+                Y=torch.Tensor([loss_l.data[0]/N, loss_c.data[0]/N,
+                    loss_l.data[0]/N+loss_c.data[0]/N]).unsqueeze(0).cpu(),
+                win=lot,
+                update='append'
+            )
+            # hacky fencepost solution for 0th epoch plot
+            if iteration == 0:
+                viz.line(
+                    X=torch.zeros((1, 3)).cpu(),
+                    Y=torch.Tensor([loc_loss, conf_loss,
+                        loc_loss + conf_loss]).unsqueeze(0).cpu(),
+                    win=epoch_lot,
+                    update=True
+                )
+        if iteration % 10000 == 0:
+            print('Saving state, iter:', iteration)
+            torch.save(ssd_net.state_dict(), 'weights/ssd{}_vid_video_{}_K_{:d}.pth'.format(
+                args.size, iteration, args.K
+            ))
+    torch.save(ssd_net.state_dict(), args.save_folder + 'vid_ssd_video_' + args.version + '_batchsize_{:d}_iter_{:d}_K_{:d}.pth'.format(args.batch_size, max_iter, args.K))
+
+
+def adjust_learning_rate(optimizer, gamma, step):
+    """Sets the learning rate to the initial LR decayed by 10 at every specified step
+    # Adapted from PyTorch Imagenet example:
+    # https://github.com/pytorch/examples/blob/master/imagenet/main.py
+    """
+    lr = args.lr * (gamma ** (step))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+if __name__ == '__main__':
+    train()
