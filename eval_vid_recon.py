93606d2450a2fe6811bd8b548b452052c4265a69
demonzyj56 <demonzyj56@gmail.com>
2018-01-18 14:07:31 +0800

    Add training methods for training with reconstruction loss.

---
+++ eval_vid_recon.py
@@ -0,0 +1,162 @@
+"""Adapted from:
+    @longcw faster_rcnn_pytorch: https://github.com/longcw/faster_rcnn_pytorch
+    @rbgirshick py-faster-rcnn https://github.com/rbgirshick/py-faster-rcnn
+    Licensed under The MIT License [see LICENSE for details]
+"""
+
+import torch
+import torch.backends.cudnn as cudnn
+from torch.autograd import Variable
+from data import VIDroot
+from data import VID_CLASSES as labelmap
+from data import VIDDetection, BaseTransform
+from ssd import build_ssd
+from ssd_recon_conf_shared import build_ssd_recon_conf_shared
+import sys
+import os
+import time
+import argparse
+import numpy as np
+import pickle
+
+
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1")
+
+parser = argparse.ArgumentParser(description='Single Shot MultiBox Detection')
+parser.add_argument('--trained_model', default='weights/vid_ssd_recon_v2_512.pth',
+                    type=str, help='Trained state_dict file path to open')
+parser.add_argument('--save_folder', default='eval/', type=str,
+                    help='File path to save results')
+parser.add_argument('--confidence_threshold', default=0.01, type=float,
+                    help='Detection confidence threshold')
+parser.add_argument('--top_k', default=5, type=int,
+                    help='Further restrict the number of predictions to parse')
+parser.add_argument('--cuda', default=True, type=str2bool,
+                    help='Use cuda to train model')
+parser.add_argument('--vid_root', default=VIDroot, help='Location of VID root directory')
+parser.add_argument('--vid_val_list', default='VID_val_frames',
+                    help='validation files found in ${vid_root}/${vid_val_list}.txt')
+parser.add_argument('--size', default=512, type=int, help='Which size to use (300/512)')
+
+args = parser.parse_args()
+
+if not os.path.exists(args.save_folder):
+    os.mkdir(args.save_folder)
+
+if args.cuda and torch.cuda.is_available():
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+else:
+    torch.set_default_tensor_type('torch.FloatTensor')
+
+dataset_mean = (104, 117, 123)
+
+
+class Timer(object):
+    """A simple timer."""
+    def __init__(self):
+        self.total_time = 0.
+        self.calls = 0
+        self.start_time = 0.
+        self.diff = 0.
+        self.average_time = 0.
+
+    def tic(self):
+        # using time.time instead of time.clock because time time.clock
+        # does not normalize for multithreading
+        self.start_time = time.time()
+
+    def toc(self, average=True):
+        self.diff = time.time() - self.start_time
+        self.total_time += self.diff
+        self.calls += 1
+        self.average_time = self.total_time / self.calls
+        if average:
+            return self.average_time
+        else:
+            return self.diff
+
+
+def get_output_dir(name, phase):
+    """Return the directory where experimental artifacts are placed.
+    If the directory does not exist, it is created.
+    A canonical path is built using the name from an imdb and a network
+    (if not None).
+    """
+    filedir = os.path.join(name, phase)
+    if not os.path.exists(filedir):
+        os.makedirs(filedir)
+    return filedir
+
+
+def test_net(save_folder, net, cuda, dataset, transform, top_k,
+             im_size=300, thresh=0.05):
+    """Test a Fast R-CNN network on an image database."""
+    num_images = len(dataset)
+    # all detections are collected into:
+    #    all_boxes[cls][image] = N x 5 array of detections in
+    #    (x1, y1, x2, y2, score)
+    all_boxes = [[[] for _ in range(num_images)]
+                 for _ in range(len(labelmap)+1)]
+
+    # timers
+    _t = {'im_detect': Timer(), 'misc': Timer()}
+    output_dir = get_output_dir('ssd{}_120000'.format(im_size), 'test')
+    det_file = os.path.join(output_dir, 'detections.pkl')
+
+    for i in range(num_images):
+        im, gt, h, w = dataset.pull_item(i)
+
+        x = Variable(im.unsqueeze(0))
+        if args.cuda:
+            x = x.cuda()
+        _t['im_detect'].tic()
+        detections = net(x).data
+        detect_time = _t['im_detect'].toc(average=False)
+
+        # skip j = 0, because it's the background class
+        for j in range(1, detections.size(1)):
+            dets = detections[0, j, :]
+            mask = dets[:, 0].gt(0.).expand(5, dets.size(0)).t()
+            dets = torch.masked_select(dets, mask).view(-1, 5)
+            if dets.dim() == 0:
+                continue
+            boxes = dets[:, 1:]
+            boxes[:, 0] *= w
+            boxes[:, 2] *= w
+            boxes[:, 1] *= h
+            boxes[:, 3] *= h
+            scores = dets[:, 0].cpu().numpy()
+            cls_dets = np.hstack((boxes.cpu().numpy(), scores[:, np.newaxis])) \
+                .astype(np.float32, copy=False)
+            all_boxes[j][i] = cls_dets
+
+        print('im_detect: {:d}/{:d} {:.3f}s'.format(i + 1,
+                                                    num_images, detect_time))
+
+    with open(det_file, 'wb') as f:
+        pickle.dump(all_boxes, f, pickle.HIGHEST_PROTOCOL)
+
+    print('Evaluating detections')
+    dataset.evaluate_detections(all_boxes)
+
+
+if __name__ == '__main__':
+    # load net
+    num_classes = len(labelmap) + 1 # +1 background
+    im_size = args.size
+    net = build_ssd_recon_conf_shared('test', im_size, num_classes) # initialize SSD
+
+    net.load_state_dict(torch.load(args.trained_model))
+    net.eval()
+    print('Finished loading model!')
+    # load data
+    dataset = VIDDetection([args.vid_val_list], 'data/', args.vid_root,
+                           transform=BaseTransform(im_size, dataset_mean), is_test=True)
+    if args.cuda:
+        net = net.cuda()
+        cudnn.benchmark = True
+    # evaluation
+    test_net(args.save_folder, net, args.cuda, dataset,
+             BaseTransform(net.size, dataset_mean), args.top_k, im_size,
+             thresh=args.confidence_threshold)
---
+++ ssd_recon_conf_shared.py
@@ -0,0 +1,249 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from layers import Detect, L2Norm, PriorBox
+from data import v2, v2_512
+import os
+
+
+# This function is derived from torchvision VGG make_layers()
+# https://github.com/pytorch/vision/blob/master/torchvision/models/vgg.py
+def vgg(cfg, i, batch_norm=False):
+    layers = []
+    in_channels = i
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        elif v == 'C':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    pool5 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+    conv6 = nn.Conv2d(512, 1024, kernel_size=3, padding=6, dilation=6)
+    conv7 = nn.Conv2d(1024, 1024, kernel_size=1)
+    layers += [pool5, conv6,
+               nn.ReLU(inplace=True), conv7, nn.ReLU(inplace=True)]
+    return layers
+
+
+def add_extras_conf_shared(cfg, i, batch_norm=False):
+    # Extra layers added to VGG for feature scaling
+    layers = []
+    in_channels = i
+    flag = False
+    for k, v in enumerate(cfg):
+        if in_channels != 'S':
+            if v == 'S':
+                layers += [nn.Conv2d(in_channels, cfg[k + 1],
+                           kernel_size=(1, 3)[flag], stride=2, padding=1)]
+            elif v == '512Last':
+                layers +=[nn.Conv2d(in_channels, 512, kernel_size=4, padding=1)]
+            else:
+                layers += [nn.Conv2d(in_channels, v, kernel_size=(1, 3)[flag])]
+            flag = not flag
+        in_channels = v
+    return layers
+
+
+def multibox_recon_conf_shared(vgg, extra_layers, cfg, num_classes, latent_channels):
+    """ Notice that conf_layers have only ONE element. """
+    # loc_layers = []
+    # recon_layers = []
+    vgg_source = [24, -2]
+    shared_channel_size = vgg[24].out_channels  # 512
+    conf_layers = [nn.Conv2d(latent_channels, max(cfg)*num_classes, kernel_size=3, padding=1)]
+    recon_layers = [
+        nn.Conv2d(shared_channel_size, latent_channels, kernel_size=1),
+        nn.Conv2d(latent_channels, shared_channel_size, kernel_size=1)
+    ]
+    loc_layers = [nn.Conv2d(latent_channels, k*4, kernel_size=3, padding=1) for k in cfg]
+    # for k, v in enumerate(vgg_source):
+    #     loc_layers += [nn.Conv2d(latent_channels,
+    #                              cfg[k] * 4, kernel_size=3, padding=1)]
+    # for k, v in enumerate(extra_layers[1::2], 2):
+    #     loc_layers += [nn.Conv2d(latent_channels, cfg[k]
+    #                              * 4, kernel_size=3, padding=1)]
+    return vgg, extra_layers, (loc_layers, conf_layers, recon_layers)
+
+
+base = {
+    '300': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512, 'M',
+            512, 512, 512],
+    '512': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512, 'M',
+            512, 512, 512],
+}
+extras_conf_shared = {
+    '300': [256, 'S', 512, 256, 'S', 512, 256, 512, 256, 512],
+    '512': [256, 'S', 512, 256, 'S', 512, 256, 'S', 512, 256, 'S', 512, 256, '512Last'],
+}
+mbox = {
+    '300': [4, 6, 6, 6, 4, 4],  # number of boxes per feature map location
+    '512': [4, 6, 6, 6, 6, 4, 4],  # number of boxes per feature map location
+}
+
+
+class SSDReconConfShared(nn.Module):
+    """Single Shot Multibox Architecture with reconstruction module.
+    The network is composed of a base VGG network followed by the
+    added multibox conv layers.  Each multibox layer branches into
+        1) conv2d for class conf scores
+        2) conv2d for localization predictions
+        3) associated priorbox layer to produce default bounding
+           boxes specific to the layer's feature map size.
+    See: https://arxiv.org/pdf/1512.02325.pdf for more details.
+
+    Args:
+        phase: (string) Can be "test" or "train"
+        base: VGG16 layers for input, size of either 300 or 500
+        extras: extra layers that feed to multibox loc and conf layers
+        head: "multibox head" consists of loc and conf conv layers
+    """
+
+    def __init__(self, phase, base, extras, head, num_classes, sz, cfg):
+        super(SSDReconConfShared, self).__init__()
+        self.phase = phase
+        self.num_classes = num_classes
+        # TODO: implement __call__ in PriorBox
+        self.priorbox = PriorBox(v2 if sz==300 else v2_512)
+        self.priors = Variable(self.priorbox.forward(), volatile=True)
+        self.size = sz
+        self.cfg = cfg
+        self.fc7_conv_512 = nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=1)
+
+        # SSD network
+        self.vgg = nn.ModuleList(base)
+        # Layer learns to scale the l2 normalized features from conv4_3
+        self.L2Norm = L2Norm(512, 20)
+        self.extras = nn.ModuleList(extras)
+
+        self.loc = nn.ModuleList(head[0])
+        self.conf = nn.ModuleList(head[1])
+        self.rec = nn.ModuleList(head[2])
+
+        if phase == 'test':
+            self.softmax = nn.Softmax()
+            self.detect = Detect(num_classes, 0, 200, 0.01, 0.45)
+
+    def forward(self, x):
+        """Applies network layers and ops on input image(s) x.
+
+        Args:
+            x: input image or batch of images. Shape: [batch,3*batch,300,300].
+
+        Return:
+            Depending on phase:
+            test:
+                Variable(tensor) of output class label predictions,
+                confidence score, and corresponding location predictions for
+                each object detected. Shape: [batch,topk,7]
+
+            train:
+                list of concat outputs from:
+                    1: confidence layers, Shape: [batch*num_priors,num_classes]
+                    2: localization layers, Shape: [batch,num_priors*4]
+                    3: priorbox layers, Shape: [2,num_priors*4]
+        """
+        sources = list()
+        loc = list()
+        conf = list()
+        rec = list()
+
+        # apply vgg up to conv4_3 relu
+        for k in range(23):
+            x = self.vgg[k](x)
+
+        s = self.L2Norm(x)
+        sources.append(s)
+
+        # apply vgg up to fc7
+        for k in range(23, len(self.vgg)):
+            x = self.vgg[k](x)
+        sources.append(x)
+
+        # apply extra layers and cache source layer outputs
+        for k, v in enumerate(self.extras):
+            x = F.relu(v(x), inplace=True)
+            if k % 2 == 1:
+                sources.append(x)
+
+        # apply multibox head to source layers
+        for idx, (x, l) in enumerate(zip(sources, self.loc)):
+            if idx == 1:
+                x = F.relu(self.fc7_conv_512(x), inplace=True)
+            latent = self.rec[0](x)
+            loc.append(l(latent).permute(0, 2, 3, 1).contiguous())
+            conf.append(
+                self.conf[0](latent)[:, :self.cfg[idx]*self.num_classes, :, :].permute(0, 2, 3, 1).contiguous()
+            )
+            rec.append(
+                torch.pow(F.relu(self.rec[1](latent), inplace=True)-x, 2).mean(dim=1, keepdim=False).contiguous()
+            )
+
+        loc = torch.cat([o.view(o.size(0), -1) for o in loc], 1)
+        conf = torch.cat([o.view(o.size(0), -1) for o in conf], 1)
+        rec = torch.cat([o.view(o.size(0), -1) for o in rec], 1)
+        if self.phase == "test":
+            output = self.detect(
+                loc.view(loc.size(0), -1, 4),                   # loc preds
+                self.softmax(conf.view(-1, self.num_classes)),  # conf preds
+                self.priors.type(type(x.data))                  # default boxes
+            )
+        else:
+            output = (
+                loc.view(loc.size(0), -1, 4),
+                conf.view(conf.size(0), -1, self.num_classes),
+                rec.view(rec.size(0), -1),
+                self.priors
+            )
+        return output
+
+    def sources(self, x):
+        """ Returns source layer. """
+        sources = list()
+
+        # apply vgg up to conv4_3 relu
+        for k in range(23):
+            x = self.vgg[k](x)
+
+        s = self.L2Norm(x)
+        sources.append(s)
+
+        # apply vgg up to fc7
+        for k in range(23, len(self.vgg)):
+            x = self.vgg[k](x)
+        sources.append(self.fc7_conv_512(x))
+
+        # apply extra layers and cache source layer outputs
+        for k, v in enumerate(self.extras):
+            x = F.relu(v(x), inplace=True)
+            if k % 2 == 1:
+                sources.append(x)
+
+        return sources
+
+    def load_weights(self, base_file):
+        other, ext = os.path.splitext(base_file)
+        if ext == '.pkl' or '.pth':
+            print('Loading weights into state dict...')
+            self.load_state_dict(torch.load(base_file, map_location=lambda storage, loc: storage))
+            print('Finished!')
+        else:
+            print('Sorry only .pth and .pkl files supported.')
+
+
+def build_ssd_recon_conf_shared(phase, size=300, num_classes=21):
+    assert phase in ('train', 'test'), 'Phase not recognized'
+
+    return SSDReconConfShared(phase, *multibox_recon_conf_shared(
+        vgg(base[str(size)], 3),
+        add_extras_conf_shared(extras_conf_shared[str(size)], 1024),
+        mbox[str(size)],
+        num_classes,
+        256
+    ), num_classes=num_classes, sz=size, cfg=mbox[str(size)])
